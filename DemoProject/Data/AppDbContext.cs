67361aeed49cd8f7e0c6285b98b203fed9494e29
arham-anees <arhamanees10@gmail.com>
2020-04-30 23:57:07 -0700

    Add project files.

---
+++ DemoProject/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+
+
+        public DbSet<cUser> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<cUser>().Property(x => x.FirstName).IsRequired().HasMaxLength(30);
+            modelBuilder.Entity<cUser>().Property(x => x.LastName).IsRequired().HasMaxLength(30);
+        }
+    }
+}
